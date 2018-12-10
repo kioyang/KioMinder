4bd1084067cfc1ac52c786eac2fb7fbf5ad2b097
kioyang <1421104933@qq.com>
2018-12-10 23:36:23 +0800

    增加typescript支持”

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,21 @@ const path = require('path');
 const ConsoleLogOnBuildWebpackPlugin = require(path.resolve(__dirname, 'ConsoleLogOnBuildWebpackPlugin.js'));
 module.exports = {
   entry: {
-    app: './src/index.js',
+    app: './src/index.ts',
   },
   output: {
       path: path.resolve(__dirname, 'dist'),
       filename: '[name].bundle.js',
   },
+    devtool: 'source-map',
+    resolve: {
+        extensions: [ ".js", ".ts"]
+    },
+    module: {
+      rules: [
+          { test: /\.ts?$/, loader: "ts-loader" }
+      ],
+    },
     mode: 'development',
     plugins: [
         new ConsoleLogOnBuildWebpackPlugin(),

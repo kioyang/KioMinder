4315f0817f968bcabf1e58be5304d0d5273fd16e
kioyang <1421104933@qq.com>
2019-01-19 17:14:56 +0800

    有bug版本

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const ConsoleLogOnBuildWebpackPlugin = require(path.resolve(__dirname, 'ConsoleLogOnBuildWebpackPlugin.js'));
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
   entry: {
     app: './src/index.ts',
@@ -8,17 +11,36 @@ module.exports = {
       path: path.resolve(__dirname, 'dist'),
       filename: '[name].bundle.js',
   },
-    devtool: 'source-map',
     resolve: {
-        extensions: [ ".js", ".ts"]
+        extensions: ['.ts', '.js']
     },
+    devtool: "inline-source-map",
     module: {
       rules: [
-          { test: /\.ts?$/, loader: "ts-loader" }
+          { test: /\.ts?$/, loader: "ts-loader" },
+          {
+              test: /\.css$/,
+              use: [
+                  'style-loader',
+                  { loader: 'css-loader', options: { importLoaders: 1 } },
+                  'postcss-loader'
+              ]
+          },
+          {
+              test: /\.(png|svg|jpg|gif)$/,
+              use: [
+                  'file-loader'
+              ]
+          },
       ],
     },
     mode: 'development',
     plugins: [
         new ConsoleLogOnBuildWebpackPlugin(),
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'KioMinder',
+            template: path.resolve(__dirname, 'src/index.html'),
+        })
     ]
 };
